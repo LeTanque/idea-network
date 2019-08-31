7bb5342c20472a230460db0ffecd760b3b2348f9
Frank Martinez <frankie.martinez@gmail.com>
2019-08-30 22:57:49 -0700

    setup PG locally with environment

--- knexfile.js
+++ knexfile.js
@@ -1,37 +1,15 @@
+// REQUIRED FOR KNEXFILE TO ACKNOWLEDGE ENV WITH LOCAL MIGRATIONS
 const dotenv = require('dotenv');
 dotenv.config();
 
 const productionDbConnection = process.env.DATABASE_URL || "localhost";
-const developmentDbFile = process.env.DB_FILE || "./data/db.db3"
 
-// console.log("SQLITE FILE in knexfile:  ---> ", process.env.DB_FILE);
-// console.log("NODE_ENV? is that a thing?  -->", process.env.NODE_ENV);
-// console.dir(process.env);
 
 module.exports = {
-  // development: {
-  //   client: 'sqlite3',
-  //   connection: {
-  //     filename: developmentDbFile,
-  //   },
-  //   migrations: {
-  //     directory: __dirname + '/data/migrations',
-  //     tableName: 'knex_migrations',
-  //   },
-  //   seeds: {
-  //     directory: __dirname + '/data/seeds',
-  //   },
-  //   pool: {
-  //     afterCreate: (conn, done) => {
-  //       conn.run('PRAGMA foreign_keys = ON', done);
-  //     },
-  //   },
-  //   useNullAsDefault: true
-  // },
   hotdev: {
     client: 'sqlite3',
     connection: {
-      filename: __dirname + '/data/idea-hotdev.db3',
+      filename: process.env.DB_FILE,
     },
     migrations: {
       directory: __dirname + '/data/migrations',
@@ -44,6 +22,18 @@ module.exports = {
       afterCreate: (conn, done) => {
         conn.run('PRAGMA foreign_keys = ON', done);
       },
+    },
+    useNullAsDefault: true
+  },
+  localpg: {
+    client: 'pg',
+    connection: `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:5432/${process.env.DB_LOCAL}`,
+    migrations: {
+      directory: __dirname + '/data/migrations',
+      tableName: 'knex_migrations',
+    },
+    seeds: {
+      directory: __dirname + '/data/seeds',
     },
     useNullAsDefault: true
   },
